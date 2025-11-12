e3cb7b7ce8f6d03e15dda564d0a2b6d1b6f09aa9
minjaehan <tommy3942@naver.com>
2025-11-13 00:23:09 +0900

    feat:id로 mapping 추가

--- src/main/java/umc/domain/member/controller/MemberController.java
+++ src/main/java/umc/domain/member/controller/MemberController.java
@@ -1,8 +1,11 @@
 package umc.domain.member.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import umc.domain.member.dto.res.MemberResDTO;
 import umc.domain.member.service.MemberService;
 
 @RestController
@@ -10,5 +13,10 @@ import umc.domain.member.service.MemberService;
 @RequestMapping("/api/members")
 public class MemberController {
     private final MemberService memberService;
+
+    @GetMapping("/{id}")
+    public MemberResDTO.Mypage getMember(@PathVariable Long id) {
+        return memberService.getById(id);
+    }
 
 }
