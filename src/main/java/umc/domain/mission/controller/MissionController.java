8f50359206015dd28b24bbccdc6361db3f32860d
minjaehan <tommy3942@naver.com>
2025-11-13 17:30:54 +0900

    fix: 의존성 정리

--- src/main/java/umc/domain/mission/controller/MissionController.java
+++ src/main/java/umc/domain/mission/controller/MissionController.java
@@ -1,10 +1,8 @@
 package umc.domain.mission.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import umc.domain.mission.dto.res.MissionResDTO;
 import umc.domain.mission.service.MissionQueryService;
